8d2a0730407d32d83183828088def378fd252fe7
r3klaw <r3klaw@localhost.localdomain>
2018-11-07 17:57:14 +0300

    added methods in DependenceGraph

--- src/test/java/dependenceAnalysis/analysis/assignment1/solution/ProgramDependenceGraph.java
+++ src/test/java/dependenceAnalysis/analysis/assignment1/solution/ProgramDependenceGraph.java
@@ -1,4 +1,25 @@
 package dependenceAnalysis.analysis.assignment1.solution;
 
+import dependenceAnalysis.util.cfg.Graph;
+import dependenceAnalysis.util.cfg.Node;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import java.util.Collection;
+
 public class ProgramDependenceGraph {
+    public ProgramDependenceGraph(ClassNode cn, MethodNode target) {
+    }
+
+    public Collection<Node> backwardSlice(Node n) {
+    }
+
+    public double computeOverlap() {
+    }
+
+    public Graph computeResult() {
+    }
+
+    public void setControlFlowGraph(Graph controlFlowGraph) {
+    }
 }
